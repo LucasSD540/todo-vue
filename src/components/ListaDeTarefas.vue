60d6405d21020555882da067e07f3fbe0ee9b53d
LucasSD540 <lucas.souzaduarte.73@gmail.com>
2023-10-19 11:19:19 -0300

    finalizando o projeto todo-list com vue js

---
+++ src/components/ListaDeTarefas.vue
@@ -0,0 +1,27 @@
+<script setup>
+const props = defineProps(['tarefas', 'tarefasPendentes']);
+</script>
+
+
+<template>
+    <ul v-if="props.tarefas.length == 0" class="list-unstyled">
+        <li>
+            <h3 class="mt-5">Não há tarefas cadastradas</h3>
+        </li>
+    </ul>
+    <ul v-else-if="props.tarefas.length > 0" class="list-group mt-4">
+        <li class="list-group-item" v-for="tarefa in props.tarefas">
+            <input @change="evento => tarefa.finalizada = evento.target.checked" :checked="tarefa.finalizada"
+                :id="tarefa.titulo" type="checkbox">
+            <label :class="{ done: tarefa.finalizada }" class="ms-3" :for="tarefa.titulo">
+                {{ tarefa.titulo }}
+            </label>
+        </li>
+    </ul>
+</template>
+
+<style scoped>
+.done {
+    text-decoration: line-through;
+}
+</style>
